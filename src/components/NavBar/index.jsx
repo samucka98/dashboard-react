4e26b5ff8cf9371e8986aeddd7a9183a9e6ae8f0
samucka98 <samuelibiapinolima@gmail.com>
2020-06-24 20:43:54 -0300

    ajuste no template dashboard

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const NavBar = () => {
+  return(
+    <div className="navBar">
+      NavBar
+    </div>
+  );
+}
+
+export default NavBar;
--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './styles.css';
 
+import SideBar from '../../components/SideBar';
+import NavBar from '../../components/NavBar';
+import Main from '../../components/Main';
+
 const Dashboard = () => {
   return(
     <div className="dashboard">
-      Hello Dash
+      <div className="dashboard__sideBar">
+        <SideBar />
+      </div>
+
+      <div className="dashboard__content">
+        <div className="dashboard__content-nav">
+          <NavBar />
+        </div>
+
+        <div className="dashboard__content-main">
+          <Main />
+        </div>
+      </div>
     </div>
   );
 }
