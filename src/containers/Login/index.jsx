0693ee83892a638450f65b9529bfe5b240d11454
samucka98 <samuelibiapinolima@gmail.com>
2020-06-24 19:36:08 -0300

    teste

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -22,7 +22,7 @@ const Login = () => {
       }).then(response => {
         setAuth(response.data);
         history.push('/dashboard');
-      })
+      });
 
       console.log();
     } catch (error) {
